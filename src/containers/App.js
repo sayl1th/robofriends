2735fb4354f1f7c3f146466c2d02eed561d8be4a
sayl1th <saylith@icloud.com>
2020-07-12 17:24:25 +0200

    fix typo

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,7 @@ class App extends Component {
         <SearchBox searchChange={this.changeOnSearch} />
         <Scroll>
           <ErrorBoundry>
-            <CardList robots={filteredRobots} />;
+            <CardList robots={filteredRobots} />
           </ErrorBoundry>
         </Scroll>
       </div>
